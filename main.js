497267984622138b9a00fdec4e366a37e4498f59
Minjee Park <minjee412@gmail.com>
2021-08-29 04:53:58 +0900

    유튜브클론코딩(실습)

---
+++ main.js
@@ -0,0 +1,7 @@
+const moreBtn = document.querySelector('.video_info .metadata .moreBtn');
+const title = document.querySelector('.video_info .metadata .title');
+
+moreBtn.addEventListener('click', () => {
+    moreBtn.classList.toggle('clicked');
+    title.classList.toggle('clamp');
+});
