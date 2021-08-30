22145a4d1273b92595b0e72fe62dc8e844fd051d
Minjee Park <minjee412@gmail.com>
2021-08-30 15:39:52 +0900

    youtube-clonecoding

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const moreBtn = document.querySelector('.video_info .metadata .moreBtn');
 const title = document.querySelector('.video_info .metadata .title');
-///
+//
 moreBtn.addEventListener('click', () => {
     moreBtn.classList.toggle('clicked');
     title.classList.toggle('clamp');
